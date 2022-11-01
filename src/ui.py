bce4e39c1a769220fc171e9906775dddac9a4a35
Rashitov Ilyas <rashtv.19@gmail.com>
2022-11-01 14:55:48 +0600

    Initial Structure of App added

---
+++ src/ui.py
@@ -0,0 +1,10 @@
+class User:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+class Library:
+    def __init__(self, id, platform):
+        self.id = id
+        self.platform = platform
+        
